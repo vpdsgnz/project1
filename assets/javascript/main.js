87f2ff7f7a8e26f2f2e8e35e523c8022365f1e9b
vpdsgnz <vpdsgnz@hotmail.com>
2019-05-04 01:02:51 -0400

    revert bracket css, add pokemon stats

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,27 +1,64 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDMfsZJnIvX4wHBJPzVeaYiyWBdLzn4eZY",
+    authDomain: "poketourney-65c90.firebaseapp.com",
+    databaseURL: "https://poketourney-65c90.firebaseio.com",
+    projectId: "poketourney-65c90",
+    storageBucket: "poketourney-65c90.appspot.com",
+    messagingSenderId: "241434009638"
+};
+firebase.initializeApp(config);
+
+    // Create a variable to reference the database.
+    // var database = firebase.database();
+
+
+
 /* Custom data objects passed as teams */
 var customData = {
     teams: [
-        [{ name: "Team 1", flag: 'fi' }, null],
-        [{ name: "Team 3", flag: 'se' }, { name: "Team 4", flag: 'us' }]
+        [{ name: "Team 1", pokeid: '1' }, null],
+        [{ name: "Team 3", pokeid: '132' }, { name: "Team 4", pokeid: '15' }],
+        [{ name: "Team 5", pokeid: '134' }, { name: "Team 6", pokeid: '155' }],
+        [{ name: "Team 7", pokeid: '23' }, null],
+        //extra 8
+        [{ name: "Team 8", pokeid: '1' }, null],
+        [{ name: "Team 9", pokeid: '1' }, { name: "Team 10", pokeid: '151' }],
+        [{ name: "Team 11", pokeid: '14' }, { name: "Team 12", pokeid: '15' }],
+        [{ name: "Team 13", pokeid: '2' }, null]
     ],
-    results: []
+    results: [
+        [
+            [
+                [3, 5],
+                [2, 4],
+                [6, 3],
+                [2, 3],
+                [1, 5],
+                [5, 3],
+                [7, 2],
+                [1, 2]
+            ],
+            [
+                [1, 2],
+                [3, 4],
+                [5, 6],
+                [7, 8]
+            ],
+            [
+                [9, 1],
+                [8, 2]
+            ],
+            [
+                [1, 3]
+            ],
+        ]
+    ]
 }
 
 /* Edit function is called when team label is clicked */
 function edit_fn(container, data, doneCb) {
-    var input = $('<input type="text">')
-    input.val(data ? data.pokemon + ':' + data.name : '')
-    container.html(input)
-    input.focus()
-    input.blur(function() {
-        var inputValue = input.val()
-        if (inputValue.length === 0) {
-            doneCb(null); // Drop the team and replace with BYE
-        } else {
-            var flagAndName = inputValue.split(':') // Expects correct input
-            doneCb({ flag: flagAndName[0], name: flagAndName[1] })
-        }
-    })
+    var input = $('<div>')
 }
 
 /* Render function is called for each team label when data is changed, data
@@ -46,8 +83,9 @@ function render_fn(container, data, score, state) {
         case "entry-no-score":
         case "entry-default-win":
         case "entry-complete":
-            container.append('<img src="https://misnina.github.io/reddit-pokemon-trade-search/regular/helioptile.png" height="16px" class="zerofit" /> ').append(data.name)
+            container.append('<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + data.pokeid + '.png" height="16px" class="zerofit" /> ').append(data.name)
             return;
+
     }
 }
 
@@ -55,10 +93,10 @@ $(function() {
     $('div#matches .demo').bracket({
         save: function() {},
         /* without save() labels are disabled */
-        teamHeight: 600,
-        scoreHeight: 600,
+        teamHeight: 800,
+        scoreHeight: 800,
         matchMargin: 10,
-        roundMargin: 60,
+        roundMargin: 50,
         init: customData,
         decorator: {
             edit: edit_fn,
@@ -66,3 +104,95 @@ $(function() {
         }
     })
 })
+
+
+
+// get pokemon names and pokeid
+var queryURL = "https://pokeapi.co/api/v2/pokedex/2/";
+var testvar;
+var pokelist = {};
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+
+    for (let i = 0; i < 150 + 1; i++) {
+
+        pokelist[response.pokemon_entries[i].pokemon_species.name] = response.pokemon_entries[i].entry_number;
+        var newopt = $("<option>");
+        newopt.text(response.pokemon_entries[i].pokemon_species.name);
+        newopt.attr({ "value": pokelist[response.pokemon_entries[i].pokemon_species.name], "data-pokeid": pokelist[response.pokemon_entries[i].pokemon_species.name] });
+        newopt.appendTo("#pokesel0");
+        //console.log(pokelist[response.pokemon_entries[i].pokemon_species.name]);
+
+        var statlist = {} = $('<li class="mdl-list__item poke-list">');
+        statlist.attr({"data-pokeid": pokelist[response.pokemon_entries[i].pokemon_species.name], "data-name": response.pokemon_entries[i].pokemon_species.name});
+        statlist.span = $('<span class="mdl-list__item-primary-content">');
+        statlist.span.html("<i> " + response.pokemon_entries[i].entry_number + ". </i>" + response.pokemon_entries[i].pokemon_species.name);
+        statlist.append(statlist.span);
+        $("#stat-list").append(statlist);
+
+    }
+    $("#pokesel0").select2({
+        theme: "material"
+    });
+
+    $(".select2-selection__arrow")
+        .addClass("material-icons")
+        .html("arrow_drop_down");
+
+});
+
+
+var querystats = "https://nanofuxion.github.io/pokemon-stats-api/api/data/";
+$.ajax({
+    url: querystats,
+    method: "GET"
+}).then(function(response) {
+    testvar = response;
+});
+
+
+$(document).ready(function() {
+
+    ////select a pokemon to enter the tourney////
+    $("#pokesel0").change(pickedpoke);
+
+    function pickedpoke() {
+        if ($(this).val() != 'Select your Pokémon:') {
+            console.log($(this).val());
+        } else {
+            //prompt to select a pokemon
+        }
+    }
+
+    $("#add-user").on("click", function(event) {
+        event.preventDefault();
+  
+
+    });
+
+
+
+    ////view pokemon stats////
+    $('li.poke-list').click(statfunc);
+
+    function statfunc() {
+        var current = $(this);
+        var spriteStr = ("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + current.attr("data-pokeid") + ".png");
+        $('.poke-sprite').attr({"src": spriteStr});
+        $("aside h4").html(current.attr("data-name"));
+        var tempholder;
+        tempholder = $("aside.main").find("span");
+        tempholder.html(testvar[current.attr("data-pokeid") - 1]["base-stats"]["defense"]);
+        tempholder = $("aside.main").find("span:first");
+        tempholder.html(testvar[current.attr("data-pokeid") - 1]["base-stats"]["attack"]);
+        tempholder = $("aside.main").find("span:last");
+        tempholder.html(testvar[current.attr("data-pokeid") - 1]["base-stats"]["stamina"]);
+    }
+    //set to pikachu by default
+   //$("#tab3").click($("#stat-list li:nth-child(25)").trigger("click"));
+
+
+});
